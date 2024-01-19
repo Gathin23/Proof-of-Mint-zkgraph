9771fbb36dfe1bb168da030b875eaa256ea97fd6
Gathin <gathint23@gmail.com>
2024-01-19 06:59:50 +0530

    Upd script file

--- script.sh
+++ script.sh
@@ -4,6 +4,6 @@
 # Then run `sh test.sh`
 
 npm run compile &&
-npm run exec -- x &&
+npm run exec -- 5052085 &&
 npm run prove -- --inputgen 5052085 0000000000000000000000007b1ef80aec6b92fe39d842accf562b30833d3250 &&
 npm run prove -- --test 5052085 0000000000000000000000007b1ef80aec6b92fe39d842accf562b30833d3250
